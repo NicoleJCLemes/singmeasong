4e3fd501c5b95804878b0f3e554dffe4ca00871c
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-07-26 20:23:48 -0300

    feat: post /recommendations tests

---
+++ back-end/tests/factories/recommendationsFactory.ts
@@ -0,0 +1,14 @@
+function createRecommendation() {
+    const recommendation = {
+        name: "Falamansa - Xote dos Milagres",
+        youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y"
+    }
+
+    return recommendation;
+};
+
+const recommendationsFactory = {
+    createRecommendation
+};
+
+export default recommendationsFactory;
